81661126116069f2b880795ae32a93f7364f9548
abnerjs <abner.js05@gmail.com>
2025-07-22 13:44:03 -0300

    feat: db migrations

---
+++ application/app/Database/Migrations/2025-07-22-163944_CreatePostsTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'comment'        => 'Foreign key to users table',
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'image' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp',
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('posts');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('posts');
+    }
+}
